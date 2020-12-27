ee24b824d74ce7a0271ecf9310fcbda1d2be0bc7
john-reiner <johnreiner1990@gmail.com>
2020-12-27 17:56:56 -0500

    adding auth

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,2 +1,10 @@
 class CommentsController < ApplicationController
+
+    def create
+        @blog = Blog.find(params[:blog_id])
+        @comment = @blog.comments.create(params[:comment].permit(:body))
+
+        redirect_to blog_path(@blog)
+    end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,8 @@
 Rails.application.routes.draw do
   resources :users
-  resources :comments
-  resources :blogs
+  resources :blogs do 
+    resources :comments
+  end
   resources :technology_topics
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 
